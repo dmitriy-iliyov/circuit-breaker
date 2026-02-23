0e4442d8d4c928793660734da34cd8dff19a85df
dmitriy-iliyov <dmitriy.iliyov@gmail.com>
2026-02-23 16:05:10 +0100

    del(refactor dir structure)

--- circuit-breaker-core/src/main/java/io/github/dmitriyiliyov/circuitbreaker/core/strategies/open/CircuitBreakerFallback.java
+++
@@ -1,5 +0,0 @@
-package io.github.dmitriyiliyov.circuitbreaker.core.strategies.open;
-
-public interface CircuitBreakerFallback {
-    <T> T get();
-}
--- circuit-breaker-core/src/main/java/io/github/dmitriyiliyov/circuitbreaker/core/strategies/open/CircuitBreakerOpenException.java
+++
@@ -1,7 +0,0 @@
-package io.github.dmitriyiliyov.circuitbreaker.core.strategies.open;
-
-public class CircuitBreakerOpenException extends RuntimeException {
-    public CircuitBreakerOpenException(String message) {
-        super(message);
-    }
-}
